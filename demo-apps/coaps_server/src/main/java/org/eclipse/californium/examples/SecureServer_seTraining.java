8f87cd6864620b1f633aafe54ea8b2ac99d31005
nosd <nosd@zhaw.ch>
2020-04-04 18:07:49 +0200

    added scpf's SecureServer_seTraining file and removed old project files from cf-secure project

---
+++ demo-apps/coaps_server/src/main/java/org/eclipse/californium/examples/SecureServer_seTraining.java
@@ -0,0 +1,149 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Matthias Kovatsch - creator and main architect
+ ******************************************************************************/
+package org.eclipse.californium.examples;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.eclipse.californium.core.CaliforniumLogger;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.Endpoint;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.core.network.interceptors.MessageTracer;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.eclipse.californium.scandium.DTLSConnector;
+import org.eclipse.californium.scandium.ScandiumLogger;
+import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
+import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
+import org.eclipse.californium.scandium.dtls.pskstore.InMemoryPskStore;
+
+
+public class SecureServer_seTraining {
+
+	static {
+		CaliforniumLogger.initialize();
+		CaliforniumLogger.setLevel(Level.CONFIG);
+		ScandiumLogger.initialize();
+		ScandiumLogger.setLevel(Level.FINER);
+	}
+
+	// allows configuration via Californium.properties
+	public static final int DTLS_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_SECURE_PORT);
+
+	private static final String TRUST_STORE_PASSWORD = "seTraining19";//"rootPass";
+	private final static String KEY_STORE_PASSWORD = "seTraining19";//"endPass";
+	private static final String KEY_STORE_LOCATION = "certs/seKeyStore.jks"; //"certs/keyStore.jks";
+	private static final String TRUST_STORE_LOCATION = "certs/seKeyStore.jks"; //"certs/trustStore.jks";
+	private static final String SERVER_ADDR = "2001:620:190:cafe::ff05";
+	
+	private static final String TOGGLE_PATH[] = {"/bin/sh", "/home/sepi/scripts/toggle_led.sh"};
+	
+	public static void main(String[] args) {
+		
+		CoapServer server = new CoapServer();
+		
+		server.add(new CoapResource("secure") {
+			@Override
+			public void handleGET(CoapExchange exchange) {
+				exchange.respond(ResponseCode.CONTENT, "hello security");
+				
+				try {
+					@SuppressWarnings("unused")
+					Process pr = Runtime.getRuntime().exec(TOGGLE_PATH);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		// ETSI Plugtest environment
+		// server.addEndpoint(new CoAPEndpoint(new DTLSConnector(new InetSocketAddress("::1", DTLS_PORT)), NetworkConfig.getStandard()));
+		// server.addEndpoint(new CoAPEndpoint(new DTLSConnector(new InetSocketAddress("127.0.0.1", DTLS_PORT)), NetworkConfig.getStandard()));
+		// server.addEndpoint(new CoAPEndpoint(new DTLSConnector(new InetSocketAddress("2a01:c911:0:2010::10", DTLS_PORT)), NetworkConfig.getStandard()));
+		// server.addEndpoint(new CoAPEndpoint(new DTLSConnector(new InetSocketAddress("10.200.1.2", DTLS_PORT)), NetworkConfig.getStandard()));
+
+		try {
+			// Pre-shared secrets
+			InMemoryPskStore pskStore = new InMemoryPskStore();
+			pskStore.setKey("Client_identity", "secretPSK".getBytes()); // from ETSI Plugtest test spec
+
+			// load the trust store
+			KeyStore trustStore = KeyStore.getInstance("JKS");
+			InputStream inTrust = SecureServer_seTraining.class.getClassLoader().getResourceAsStream(TRUST_STORE_LOCATION);
+			trustStore.load(inTrust, TRUST_STORE_PASSWORD.toCharArray());
+
+			// You can load multiple certificates if needed
+			Certificate[] trustedCertificates = new Certificate[2];
+			trustedCertificates[0] = trustStore.getCertificate("ines_ca"); //"root"
+			trustedCertificates[1] = trustStore.getCertificate("ca_chain"); //"chain of the InES root and the Infineon root"
+
+			// load the key store
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			InputStream in = SecureServer_seTraining.class.getClassLoader().getResourceAsStream(KEY_STORE_LOCATION);
+			keyStore.load(in, KEY_STORE_PASSWORD.toCharArray()); // KEY_STORE_PASSWORD
+
+			DtlsConnectorConfig.Builder config = new DtlsConnectorConfig.Builder();
+			config.setAddress(new InetSocketAddress(SERVER_ADDR, DTLS_PORT));
+			config.setSupportedCipherSuites(new CipherSuite[]{CipherSuite.TLS_PSK_WITH_AES_128_CCM_8,
+					CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8});
+			config.setPskStore(pskStore);
+			config.setIdentity((PrivateKey)keyStore.getKey("ines_server", KEY_STORE_PASSWORD.toCharArray()),
+					keyStore.getCertificateChain("ines_server"), false); //server instead of "1"
+			config.setTrustStore(trustedCertificates);
+			
+			config.setRetransmissionTimeout(10000);
+			config.setMaxRetransmissions(3);
+			config.setClientAuthenticationRequired(true);
+
+			DTLSConnector connector = new DTLSConnector(config.build());
+
+			server.addEndpoint(new CoapEndpoint(connector, NetworkConfig.getStandard()));
+			server.start();
+
+		} catch (GeneralSecurityException | IOException e) {
+			System.err.println("Could not load the keystore");
+			e.printStackTrace();
+		}
+
+		// add special interceptor for message traces
+		for (Endpoint ep : server.getEndpoints()) {
+			ep.addInterceptor(new MessageTracer());
+		}
+
+		System.out.println("Secure CoAP server powered by Scandium (Sc) is listening on port " + DTLS_PORT);
+	}
+
+}
